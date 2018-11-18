16de9c59fd5da954a08fca1985cef58b166ed3be
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-11-18 21:08:27 +0100

    Removed ValuesController and added method for sending email

---
+++ BalticMarinasEmailWS/Controllers/EmailController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BalticMarinasEmailWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        // GET api/event/5
+        [HttpGet("{id}")]
+        public void Send(int id)
+        {
+            //EventsContext context = HttpContext.RequestServices.GetService(typeof(BalticMarinasEventsWS.Models.EventsContext)) as EventsContext;
+            //context.DeleteEventById(id);
+        }
+    }
+}
